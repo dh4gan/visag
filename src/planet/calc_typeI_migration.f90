04e9ceae4ea7597b3c05a69a1e10b615baba6119
Duncan Forgan <dhf3@st-andrews.ac.uk>
2018-05-31 14:56:33 +0100

    Testing other Type I migration timescales

--- src/planet/calc_typeI_migration.f90
+++ src/planet/calc_typeI_migration.f90
@@ -19,7 +19,9 @@ subroutine calc_typeI_migration(iplanet, tmig1)
 
   mdiscmig = pi*ap(iplanet)*ap(iplanet)*sigma(iplanetrad(iplanet))
 
-  tmig1 = mstar*mstar*aspectratio*aspectratio/(mdiscmig*mp(iplanet)*omegaK(iplanetrad(iplanet)))
+  !tmig1 = mstar*mstar*aspectratio*aspectratio/(mdiscmig*mp(iplanet)*omegaK(iplanetrad(iplanet)))
+
+  tmig1 = aspectratio*mstar/(mp(iplanet)*omegaK(iplanetrad(iplanet)))
 
   !print*, iplanetrad(iplanet),tmig1/yr, aspectratio,mp/mstar, mdiscmig/mstar, sigma(iplanetrad(iplanet))
   
